917b8e2fc3c0721c2523b5837881a3d32613bc3a
kanitsharma <kanitsharma@protonmail.com>
2022-09-19 19:33:56 +0530

    Reference closure issue

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,90 @@
-use iced::Settings;
-use iced::pure::widget::{Button, Column, Container, Text};
-use iced::pure::Sandbox;
+// use std::process::Command;
+
+mod steps;
+
+use std::clone;
+
+use iced::widget::{button, Button, Column, Container, Text};
+use iced::{alignment, Length, Sandbox};
+use iced::{Element, Renderer, Row, Settings};
 
 fn main() -> Result<(), iced::Error> {
     Counter::run(Settings::default())
 }
 
+#[derive(Clone)]
 struct Counter {
-    count: i32,
+    todo_list: Vec<String>,
+    add_todo_button: button::State,
+    delete_todo_button: button::State,
 }
 
-#[derive(Debug, Clone, Copy)]
-enum CounterMessage {
-    Increment,
-    Decrement,
+#[derive(Debug, Clone)]
+enum TodoMessage {
+    AddTodo(String),
+    DeleteTodo(usize),
+}
+
+fn button<'a, Message: Clone>(state: &'a mut button::State, label: &str) -> Button<'a, Message> {
+    Button::new(
+        state,
+        Text::new(label).horizontal_alignment(alignment::Horizontal::Center),
+    )
+    .padding(12)
+    .width(Length::Units(100))
 }
 
 impl Sandbox for Counter {
-    type Message = CounterMessage;
+    type Message = TodoMessage;
 
     fn new() -> Self {
-        Counter { count: 0 }
-    }
-
-    fn title(&self) -> String {
-        String::from("Counter app")
-    }
-
-    fn update(&mut self, message: Self::Message) {
-        match message {
-            CounterMessage::Increment => self.count += 1,
-            CounterMessage::Decrement => self.count -= 1,
+        Counter {
+            todo_list: vec![String::from("Dummy Task")],
+            add_todo_button: button::State::new(),
+            delete_todo_button: button::State::new(),
         }
     }
 
-    fn view(&self) -> iced::pure::Element<Self::Message> {
-        let label = Text::new(format!("Count: {}", self.count));
-        let incr = Button::new("Increment").on_press(CounterMessage::Increment);
-        let decr = Button::new("Decrement").on_press(CounterMessage::Decrement);
-        let col = Column::new().push(incr).push(label).push(decr);
-        Container::new(col).center_x().center_y().width(iced::Length::Fill).height(iced::Length::Fill).into()
+    fn title(&self) -> String {
+        String::from("TodoList")
+    }
+
+    fn update(&mut self, message: TodoMessage) {
+        match message {
+            TodoMessage::AddTodo(x) => self.todo_list.push(x),
+            TodoMessage::DeleteTodo(i) => {
+                self.todo_list.swap_remove(i);
+                return;
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<TodoMessage> {
+        let tasks =
+            self.todo_list
+                .iter()
+                .enumerate()
+                .fold(Row::new().spacing(20),|row, (i, task)| {
+        
+                    let task = Text::new(task);
+                    
+                    let delete_todo = button(&mut self.delete_todo_button, "Remove").on_press(TodoMessage::DeleteTodo(i));
+                    row.push(task).push(delete_todo)
+                });
+
+        let add_todo = button(&mut self.add_todo_button, "Add Todo")
+            .on_press(TodoMessage::AddTodo(String::from("New Task")));
+        // Container::new(col)
+        //     .center_x()
+        //     .center_y()
+        //     .width(iced::Length::Fill)
+        //     .height(iced::Length::Fill).into()
+        Column::new()
+            .max_width(540)
+            .spacing(20)
+            .padding(20)
+            .push(tasks)
+            .push(add_todo)
+            .into()
     }
 }
